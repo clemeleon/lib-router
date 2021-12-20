20d82e6f88ad7d06da268f6e6a6f9b6ec34e6ec0
Clemeleon <clem@clemeleon.com>
2021-12-20 18:50:52 +0200

    added any and all and format the list of routes

--- app/Router.php
+++ app/Router.php
@@ -4,57 +4,91 @@ declare(strict_types=1);
 
 namespace App;
 
+use Exception;
+
 class Router
 {
-    private array $handlers;
-    private const METHOD_POST = 'POST';
-    private const METHOD_GET = 'GET';
-    private const METHOD_PUT = 'PUT';
-    private const METHOD_DELETE = 'DELETE';
+    private array $methods = ['GET', 'POST', 'PUT', 'DELETE'];
+
+    private array $routes = [];
 
     public function get(string $path, $handler): void
     {
-        $this->useHandler(self::METHOD_GET, $path, $handler);
+        $this->setRoute("GET", $path, $handler);
     }
 
     public function post(string $path, $handler): void
     {
+        $this->setRoute("POST", $path, $handler);
     }
 
     public function put(string $path, $handler): void
     {
+        $this->setRoute("PUT", $path, $handler);
     }
 
     public function delete(string $path, $handler): void
     {
+        $this->setRoute("DELETE", $path, $handler);
     }
 
-    private function useHandler(string $method, string $path, $handler)
+    /**
+     * @throws Exception
+     */
+    public function any(array $methods, string $path, $handler): void
     {
-        $this->handlers[$method . $path] = [
+        foreach ($methods as $method) {
+            $temp = strtoupper($method);
+            if (!in_array($temp, $this->methods)) {
+                throw new Exception("this $method is not allowed!");
+            }
+            $this->setRoute($temp, $path, $handler);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function all(string $path, $handler): void
+    {
+        foreach ($this->methods as $method) {
+            if (!in_array($method, $this->methods)) {
+                throw new Exception("this $method is not allowed!");
+            }
+            $this->setRoute($method, $path, $handler);
+        }
+    }
+
+    private function setRoute(string $method, string $path, $handler)
+    {
+        $this->routes[$method] = [
             'path' => $path,
-            'method' => $method,
             'handler' => $handler
         ];
     }
 
+    /**
+     * @throws Exception
+     */
     public function process()
     {
         $requestUri = parse_url($_SERVER['REQUEST_URI']);
         $requestPath = $requestUri['path'];
         $method = $_SERVER['REQUEST_METHOD'];
-
+        $routes = $this->routes[$method] ?? [];
         $callback = null;
-        foreach ($this->handlers as $handler){
-            if($handler['path'] === $requestPath && $method === $handler['method']){
-                $callback = $handler['handler'];
+        foreach ($routes as $route){
+            if($route['path'] === $requestPath){
+                $callback = $route['handler'];
             };
+        }
+
+        if (is_null($callback)) {
+            throw new Exception("Route not found!");
         }
 
         call_user_func_array($callback, [
             $_REQUEST
         ]);
-
-        //var_dump($requestPath);
     }
 }
